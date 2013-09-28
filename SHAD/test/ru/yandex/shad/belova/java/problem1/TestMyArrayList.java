6f3fcc6b11a20dbdaab8ee118a169275d1fcb881
Ilariia Belova <ilariyabelova@gmail.com>
2013-09-28 17:07:12 +0300

    Add tests to TestMyArrayList

--- SHAD/test/ru/yandex/shad/belova/java/problem1/TestMyArrayList.java
+++ SHAD/test/ru/yandex/shad/belova/java/problem1/TestMyArrayList.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import org.junit.Before;
@@ -27,17 +28,52 @@ public class TestMyArrayList {
     
     @Test
     public void testGetCapacity(){
-        fail();
+        MyArrayList m1 = new MyArrayList();
+        assertEquals("Fail on default capacity", 10, m1.getCapacity());
+        MyArrayList m2 = new MyArrayList(0);
+        assertEquals("Fail on default capacity", 0, m2.getCapacity());
+        MyArrayList m3 = new MyArrayList(100);
+        assertEquals("Fail on default capacity", 100, m3.getCapacity());
+    }
+    
+    @Test
+    public void testConstructorException(){
+        exception.expect(IllegalArgumentException.class);
+        new MyArrayList(-3);
     }
     
     @Test
     public void testEnsureCapacity(){
+//        MyArrayList m3 = new MyArrayList(100);
+//        m3.ensureCapacity(125);
+//        assertEquals("Fail increase", 125, m3.getCapacity());
+//        MyArrayList m4 = new MyArrayList(100);
+//        m4.ensureCapacity(99);
+//        assertEquals("Fail not insrease", 100, m4.getCapacity());
+        fail();
+    }
+    
+    @Test
+    public void testEnsureCapacityException(){
+//        exception.expect(IllegalArgumentException.class);
+//        MyArrayList m3 = new MyArrayList(100);
+//        m3.ensureCapacity(-5);
         fail();
     }
     
     @Test
     public void testRemoveInCapacityTerms(){
-        fail();
+        MyArrayList m = new MyArrayList(12);
+        m.addAll(new Object[]{1,2,3,4,5,6,7,8,9,10,11,12});
+        m.remove(3);
+        assertEquals("Failed capacity decreasing", 11, m.getCapacity());
+    }
+    
+    @Test
+    public void testAddInCapacityTerms(){
+        MyArrayList m = new MyArrayList(1);
+        m.addAll(new Object[]{1,2,3});
+        assertEquals("Failed capacity increasing", 3, m.getCapacity());
     }
     
 }
