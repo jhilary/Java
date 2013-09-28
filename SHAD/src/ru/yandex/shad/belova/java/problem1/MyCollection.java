d8640be64bbb781b88535d2e2f617a4316608049
Ilariia Belova <ilariyabelova@gmail.com>
2013-09-28 16:00:02 +0300

    Add MyCollection Skeleton

---
+++ SHAD/src/ru/yandex/shad/belova/java/problem1/MyCollection.java
@@ -0,0 +1,22 @@
+package ru.yandex.shad.belova.java.problem1;
+
+public class MyCollection {
+    //TODO: Add private constructor
+    
+    public static void sort(MyArrayList list){
+        //TODO
+    }
+    
+    public static void copy(MyLinkedList dest, MyLinkedList src){
+        //TODO
+    }
+    
+    public static void reverse(MyLinkedList list){
+        //TODO
+    }
+    
+    public static int binarySearch(MyArrayList list, Object key){
+        //TODO
+        return 0;
+    }
+}
