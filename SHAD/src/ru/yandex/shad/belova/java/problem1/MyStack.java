98f120a57dccc96f748820e80056c1b2194bb00a
Ilariia Belova <ilariyabelova@gmail.com>
2013-09-28 15:53:06 +0300

    Add MyStack skeleton

---
+++ SHAD/src/ru/yandex/shad/belova/java/problem1/MyStack.java
@@ -0,0 +1,22 @@
+package ru.yandex.shad.belova.java.problem1;
+
+public class MyStack {
+    private MyLinkedList list;
+    
+    public MyStack(){
+        //TODO
+    }
+    public void push(Object e){
+        //TODO
+    }
+    
+    public Object pop(){
+        //TODO
+        return new Object();
+    }
+    
+    public Object peek(){
+        //TODO
+        return new Object();
+    }
+}
