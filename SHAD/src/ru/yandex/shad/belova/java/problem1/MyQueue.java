c105d30b2c7813bf7c842851a22064e90078c781
Ilariia Belova <ilariyabelova@gmail.com>
2013-09-28 15:49:13 +0300

    Add queue skeleton

---
+++ SHAD/src/ru/yandex/shad/belova/java/problem1/MyQueue.java
@@ -0,0 +1,22 @@
+package ru.yandex.shad.belova.java.problem1;
+
+public class MyQueue extends MyLinkedList {
+    private MyLinkedList list;
+    
+    public MyQueue(){
+        //TODO
+    }
+    public void offer(Object e){
+        //TODO
+    }
+    
+    public Object peek(){
+        //TODO
+        return new Object();
+    }
+    
+    public Object poll(){
+        //TODO
+        return new Object();
+    }
+}
