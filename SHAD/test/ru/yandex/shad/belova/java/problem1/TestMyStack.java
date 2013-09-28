4c001fc83e3ba4ab65cdca321abe824a856b3246
Ilariia Belova <ilariyabelova@gmail.com>
2013-09-28 17:25:36 +0300

    Add tests to TestMyStack

--- SHAD/test/ru/yandex/shad/belova/java/problem1/TestMyStack.java
+++ SHAD/test/ru/yandex/shad/belova/java/problem1/TestMyStack.java
@@ -27,20 +27,45 @@ public class TestMyStack {
     
     @Test
     public void testPush(){
-        //TODO
+//        stack.push(2);
+//        assertArrayEquals("Fail offering in empty stack", new Object[]{2}, stack.toArray());
+//        stack.push(4);
+//        assertArrayEquals("Fail offering in queue", new Object[]{2,4}, stack.toArray());
         fail();
     }
     
     @Test
     public void testPeek(){
-        //TODO
+//        stack.push(3);
+//        stack.push(10);
+//        stack.push(145);
+//        stack.push(-1);
+//        assertEquals("Wrong element peeked", -1, stack.peek());
+//        assertArrayEquals("Peek have changed array", new Object[]{3,10,145,-1}, stack.toArray());
         fail();
     }
     
     @Test
+    public void testPeekException(){
+        exception.expect(NoSuchElementException.class);
+        stack.peek();
+    }
+    
+    @Test
     public void testPop(){
-        //TODO
+//        stack.push(3);
+//        stack.push(10);
+//        stack.push(145);
+//        stack.push(-1);
+//        assertEquals("Wrong element peeked", -1, stack.pop());
+//        assertArrayEquals("Peek have changed array", new Object[]{3,10,145}, stack.toArray());
         fail();
+    }
+    
+    @Test
+    public void testPopException(){
+        exception.expect(NoSuchElementException.class);
+        stack.pop();
     }
     
 }
